11224c8799863e09bdb3733965049f229cc9af54
Ayush <jha.4@iitj.ac.in>
2024-01-31 00:53:23 +0530

    created API for sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ require('../db')
 
 //Handle Error
 const handleErrors = (err) => {
-   console.log(err.message, err.code);
+   // console.log(err.message, err.code);
    let errors = { email: '', password: ''};
 
    //duplicate error code
@@ -39,29 +39,60 @@ module.exports.signup_post = async (req, res) => {
    if(!isEmailValid){
       return res.status(403).json({error: "Invalid Email"})
    }
-   bcrypt.hash(password, 12)
-   .then((hashedPassword)=>{
-      const newUser = new userModel({email, password:hashedPassword});
-      newUser.save()
-      .then((data) => {
-         return res.status(200).json({message: "Signed Up successfully"})
-      })
-      .catch((err) => {
-         // console.log("1")
-         // console.log(err)
-         return res.json({error: err});
-      })
+   const hashedPassword = await bcrypt.hash(password, 12);
+      
+   const newUser = new userModel({email, password: hashedPassword});
+   userModel.findOne({email})
+   .then((data) => {
+      if(!data) {
+         // try{
+            newUser.save()
+            .then((data) => {
+               return res.status(200).json({message: "Signed Up Successfully"});
+            })
+            .catch((err) => {
+               // console.log(err)
+               return res.status(400).json({error: err});
+            })
+         // }
+         // catch{
+         //    console.log(err)
+         //    return res.status(500).json({error: err});
+         // }
+      }
+      else{
+         return res.status(409).json({error:"Email already in use."});
+      }
    })
    .catch((err) => {
-      // console.log("2")
-      // console.log(err)
-      return res.status(500).json({error: err})
+      return res.json({error: err});
    })
+
+   
+   // const hashedPassword = await bcrypt.hash(password, 12);
+
+   // .then((hashedPassword)=>{
+   //    const newUser = new userModel({email, password:hashedPassword});
+   //    newUser.save()
+   //    .then((data) => {
+   //       return res.status(200).json({message: "Signed Up successfully"})
+   //    })
+   //    .catch((err) => {
+   //       // console.log("1")
+   //       // console.log(err)
+   //       return res.json({error: err});
+   //    })
+   // })
+   // .catch((err) => {
+   //    // console.log("2")
+   //    // console.log(err)
+   //    return res.status(500).json({error: err})
+   // })
 }
 
 module.exports.login_post = async (req, res) => {
    const {email, password} = req.body;
-   console.log(email, password);
+   // console.log(email, password);
 
    res.send('user login');
 }
